dd7ee0c49288df82a65701dd3d2196ef83c1bfaa
Kyler Johnson <kylerljohnson@icloud.com>
2020-01-11 21:47:38 -0500

    setting up configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+import configuration from '../app-config.json';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-config';
+  config = configuration;
 }
